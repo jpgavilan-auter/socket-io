1c95d5dad59ecd9237916682d9ec728ffc492bb8
Juan Pablo Gavilán <jpgavilan@auter.cl>
2022-06-10 17:08:41 -0400

    ele

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import { connectMongo } from "./db";
 connectMongo();
 
 const server = http.createServer(app);
-const httpServer = server.listen(3000);
+const httpServer = server.listen(process.env.PORT || 3000);
 const io = new WebsocketServer(httpServer);
 
 
-console.log("Server is running on port 3000");
+console.log("Server is running on port ",process.env.PORT || 3000);
 sockets(io);
